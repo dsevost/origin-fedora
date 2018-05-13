9f40d3bef0e8fb56dd15fee6334f822dd0bb1fff
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2018-05-13 21:29:36 +0300

    images push script added

---
+++ push-to-registry.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VER=${IMAGE_VERSION:-v3.9}
+
+REGISTRY_PREFIX_LOCAL=${REGISTRY_PREFIX_LOCAL:-"docker-registry.default.svc:5000/origin-images-builder"}
+REGISTRY_PREFIX_REMOTE=${REGISTRY_PREFIX_REMOTE:-"quay.io/dsevosty"}
+
+IMAGES="\
+origin-pod
+etcd \
+cli \
+origin \
+origin-deployer \
+origin-docker-builder \
+origin-sti-builder \
+node \
+openvswitch \
+"
+
+for i in $IMAGES ; do
+    skopeo copy \
+	docker://$REGISTRY_PREFIX_LOCAL/$i:$VER \
+	docker://$REGISTRY_PREFIX_REMOTE/$i:$VER
+done
