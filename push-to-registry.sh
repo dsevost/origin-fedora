4742b1e4b2b8b505d2fc461db4fc87bd3a9f68ae
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2018-05-19 22:56:18 +0300

    add image prefix

--- push-to-registry.sh
+++ push-to-registry.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-VER=${IMAGE_VERSION:-v3.9}
+VER=${IMAGE_VERSION:-v3.9.0}
+
+BUILD_PREFIX=${BUILD_PREFIX:-"fc-27-"}
+PUSH_PREFIX=${PUSH_PREFIX:-$BUILD_PREFIX}
 
 REGISTRY_PREFIX_LOCAL=${REGISTRY_PREFIX_LOCAL:-"docker-registry.default.svc:5000/origin-images-builder"}
 REGISTRY_PREFIX_REMOTE=${REGISTRY_PREFIX_REMOTE:-"quay.io/dsevosty"}
@@ -22,6 +25,7 @@ openvswitch \
 
 for i in $IMAGES ; do
     skopeo copy \
-	docker://$REGISTRY_PREFIX_LOCAL/$i:$VER \
-	docker://$REGISTRY_PREFIX_REMOTE/$i:$VER
+	docker://$REGISTRY_PREFIX_LOCAL/${BUILD_PREFIX}$i:$VER \
+	docker://$REGISTRY_PREFIX_REMOTE/${PUSH_PREFIX}$i:$VER
 done
+
